ed98f4ec096a50b79a0a8fd8d12e5d5832cb27c1
Scott Havens <scotthavens42@gmail.com>
2018-11-05 07:07:55 -0700

    Added a grouped table view to display a table with multiple station data

--- AvalancheAPI.php
+++ AvalancheAPI.php
@@ -14,15 +14,19 @@ class AvalancheAPI
     {
         $apiDefaults = [
             "avy_org" => "https://api.avalanche.org",
-            "wx_maps" => "https://cdn.snowobs.com/nac"
+            "wx_maps" => "https://cdn.snowobs.com/nac-prod",
+            "station_group_tables" => "https://cdn.snowobs.com/group_tables",
+            "station_group_tables_config" => false
         ];
-        
+
         $config = require __DIR__ . '/config.php';
         $this->baseURL = (isset($config['avy_org'])) ? $config['avy_org'] : $apiDefaults['avy_org'];
         $this->centerID = $config['center_id'];
         $this->token = $config['token'];
         $this->version = "v1";
         $this->wxBaseUrl = (isset($config['wx_base_url'])) ? $config['wx_base_url'] : $apiDefaults['wx_maps'];
+        $this->tableBaseUrl = (isset($config['station_group_tables'])) ? $config['station_group_tables'] : $apiDefaults['station_group_tables'];
+        $this->groupTablesConfig = (isset($config['station_group_tables_config'])) ? $config['station_group_tables_config'] : $apiDefaults['station_group_tables_config'];
     }
 
     /**
@@ -36,10 +40,10 @@ class AvalancheAPI
     {
         $url = $this->baseURL . "/" . $this->version . "/" . $route;
 
-        // create curl resource 
-        $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, $url); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        // create curl resource
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
@@ -51,10 +55,10 @@ class AvalancheAPI
             curl_setopt($ch,CURLOPT_POSTFIELDS, $postString);
         }
 
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        // close curl resource to free up system resources 
-        curl_close($ch); 
+        // $output contains the output string
+        $output = curl_exec($ch);
+        // close curl resource to free up system resources
+        curl_close($ch);
 
         return $output;
     }
@@ -62,7 +66,7 @@ class AvalancheAPI
     /**
     *   Description: Renders a view - returns as string
     *   @param - $path to the view file
-    *   @param - $params - any variables to be used within the view file. They must be 
+    *   @param - $params - any variables to be used within the view file. They must be
     *   referenced within the $params array
     *   @return $output - the results of the import
     */
@@ -86,7 +90,7 @@ class AvalancheAPI
     *       example: ['basemap_color' => 'lightColor', 'zoom_level'=>8, 'danger_scale' => 'top', 'map_height' => 500]
     *   @return $output - a google map in html/javascript
     */
-    public function getMap($options = []) 
+    public function getMap($options = [])
     {
         return $this->renderView(
             __DIR__ . DIRECTORY_SEPARATOR . 'views/forecast_map.php',
@@ -95,39 +99,55 @@ class AvalancheAPI
                 'base_url' => $this->baseURL,
                 'options' => $options
             ]
-        ); 
+        );
     }
 
     /**
-    *   Description: Updates the forecast on avalanche.org for all zones 
-    *   affiliated with the avalnche center. Used for the national map 
+    *   Description: Updates the forecast on avalanche.org for all zones
+    *   affiliated with the avalnche center. Used for the national map
     *   and the embedded map. Used for both creating and updating
     *   @param - $centerID the abbreviation ex. CAIC
     *   @return $output - the results of the import
     */
-    public function updateForecast() 
+    public function updateForecast()
     {
         $centerId = $this->center_id;
         // $output from API call
-        return json_decode($this->curl("forecast/import-data/$centerID")); 
+        return json_decode($this->curl("forecast/import-data/$centerID"));
     }
 
     /**
-    *   Description: Weather charts and maps - registeres necessary assets and components 
+    *   Description: Weather charts and maps - registeres necessary assets and components
     *   @return $output - the results of the import
     */
-    public function getWeatherMap() 
+    public function getWeatherMap()
     {
         return $this->renderView(
             __DIR__ . DIRECTORY_SEPARATOR . 'views/weather_map.php',
             [
-                'center_id' => $this->centerID, 
+                'center_id' => $this->centerID,
                 'token' => $this->token,
                 'wx_base_url' => $this->wxBaseUrl
             ]
         );
     }
 
+    /**
+    *   Description: Weather charts and maps - registeres necessary assets and components
+    *   @return $output - the results of the import
+    */
+    public function getNWACTables($table_name)
+    {
+        return $this->renderView(
+            __DIR__ . DIRECTORY_SEPARATOR . 'views/group_table.php',
+            [
+                'center_id' => $this->centerID,
+                'token' => $this->token,
+                'station_group_tables' => $this->tableBaseUrl,
+                'station_group_tables_config' => $this->groupTablesConfig[$table_name]
+            ]
+        );
+    }
 
 
 }
